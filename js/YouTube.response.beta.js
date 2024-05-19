900d5ee65c348f515a6e2fd2961e115d296c8c7c
Virgil Clyne <Virgil@nanocat.me>
2024-05-19 23:14:11 +0800

    test(YouTube): response
    Update YouTube.response.beta.js

--- js/YouTube.response.beta.js
+++ js/YouTube.response.beta.js
@@ -919,7 +919,7 @@ function setENV(name, platforms, database) {
 	return { Settings, Caches, Configs };
 }
 
-const $ = new ENV("🍟 GetSomeFries: ▶️ YouTube v0.1.0(1004) response.beta");
+const $ = new ENV("🍟 GetSomeFries: ▶️ YouTube v0.1.0(1031) response.beta");
 
 /***************** Processing *****************/
 // 解构URL
@@ -933,10 +933,12 @@ const FORMAT = ($response.headers?.["Content-Type"] ?? $response.headers?.["cont
 $.log(`⚠ FORMAT: ${FORMAT}`, "");
 !(async () => {
 	const { Settings, Caches, Configs } = setENV("GetSomeFries", "YouTube", Database$1);
-	$.log(`⚠ ${$.name}`, `Settings.Switch: ${Settings?.Switch}`, "");
+	$.log(`⚠ Settings.Switch: ${Settings?.Switch}`, "");
 	switch (Settings.Switch) {
 		case true:
 		default:
+			// 创建空数据
+			let body = {};
 			// 格式判断
 			switch (FORMAT) {
 				case undefined: // 视为无body
@@ -950,46 +952,69 @@ $.log(`⚠ FORMAT: ${FORMAT}`, "");
 				case "application/vnd.apple.mpegurl":
 				case "audio/mpegurl":
 					//body = M3U8.parse($response.body);
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
 					//$response.body = M3U8.stringify(body);
 					break;
-				case "text/xml":
 				case "text/html":
+					$.log(`🚧 $response.body: ${$response.body}`, "");
+					document.write($response.body);
+					// 路径判断
+					switch (PATH) {
+						case "/watch":
+							//$response.body = $response.body.replace('"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH",','"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH","controlsType":"3",');
+							//$response.body = $response.body.replace('"platform":"DESKTOP",','"platform":"MOBILE",');
+							//let script = document.querySelector("head > script:nth-child(31)");
+							let script = document.head.querySelector("script:nth-child(22)");
+							//$.log(`🚧 script: ${script.outerHTML}`, "");
+							$.log(`🚧 script: ${script.innerHTML}`, "");
+							const CFGRegex = /ytcfg\.set\((?<ytcfg>{.+})\)/;
+							let ytcfg = script.innerHTML.match(CFGRegex)?.groups.ytcfg || null;
+							$.log(`🚧 ytcfg: ${ytcfg}`, "");
+							//document.getElementById("movie_player").classList.add("ytp-hide-controls");
+							//document.getElementById("movie_player").classList.add("ytp-native-controls");
+							//document.getElementById("movie_player").classList.remove("ytp-branding-shown");
+							//document.getElementsByTagName("video")[0].controls = true;
+							//document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls").remove()
+							//document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container")
+							//class="branding-img-container ytp-button"
+							//#movie_player > div:nth-child(30) > div > button
+							//let script = body.querySelector("body > script:nth-child(10)").textContent;
+							//let script = document.createElement("script");
+							//$.log(`🚧 nonce: ${document.querySelector('head > script:nth-child(1)').nonce}`, "");
+							//script.nonce = document.querySelector('head > script:nth-child(1)').nonce;
+							//script.innerHTML = "window.yt.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].controlsType = 3; window.yt.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].platform = 'MOBILE';";
+							//$.log(`🚧 newScript: ${script.outerHTML}`, "");
+							//let newScript = document.adoptNode(script);
+							//for (const child of document.documentElement.childNodes) {
+							//	$.log(`🚧 child: ${child.outerHTML}`, "")
+							//};
+							//$.log(`🚧 body: ${document.getElementsByTagName('body')[0].outerHTML}`, "");
+							//document.body.prepend(script);
+							//body.querySelector("body > script:nth-child(10)").textContent = "window.yt.config_.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].controlsType = 3; window.yt.config_.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].platform = 'MOBILE';" + script;
+							break;
+					}					$response.body = document.documentElement.outerHTML;
+					break;
+				case "text/xml":
 				case "text/plist":
 				case "application/xml":
 				case "application/plist":
 				case "application/x-plist":
-					$.log(`🚧 ${$.name}`, `body: ${$response.body}`, "");
-					//body = new DOMParser().parseFromString($response.body, FORMAT);
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
-					// 路径判断
-					switch (PATH) {
-						case "/watch":
-							$response.body = $response.body.replace('"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH",','"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH","controlsType":"3",');
-							$response.body = $response.body.replace('"platform":"DESKTOP",','"platform":"MOBILE",');
-							//let script = body.querySelector("head > script:nth-child(22)");
-							//$.log(`🚧 ${$.name}`, `script: ${JSON.stringify(script)}`, "");
-							//$.log(`🚧 ${$.name}`, `cgiData: ${JSON.stringify(cgiData ?? undefined)}`, "");
-							//document.getElementById("movie_player").classList.add("ytp-hide-controls");
-							//document.getElementById("movie_player").classList.add("ytp-native-controls");
-							//document.getElementById("movie_player").classList.remove("ytp-branding-shown");
-							//document.getElementById("video").classList.remove("ytp-branding-shown");
-							//class="branding-img-container ytp-button"
-							//#movie_player > div:nth-child(30) > div > button
-							break;
-					}					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
-					//$response.body = new XMLSerializer().serializeToString(body);
+					//$.log(`🚧 $response.body: ${$response.body}`, "");
+					body = new DOMParser().parseFromString($response.body, FORMAT);
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
+					$response.body = new XMLSerializer().serializeToString(body);
+					//$.log(`🚧 $response.body: ${$response.body}`, "");
 					break;
 				case "text/vtt":
 				case "application/vtt":
 					//body = VTT.parse($response.body);
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
 					//$response.body = VTT.stringify(body);
 					break;
 				case "text/json":
 				case "application/json":
 					//body = JSON.parse($request.body ?? "{}");
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
 					//$request.body = JSON.stringify(body);
 					break;
 				case "application/protobuf":
--- src/YouTube.response.beta.js
+++ src/YouTube.response.beta.js
@@ -5,7 +5,7 @@ import ENV from "./ENV/ENV.mjs";
 import Database from "./database/index.mjs";
 import setENV from "./function/setENV.mjs";
 
-const $ = new ENV("🍟 GetSomeFries: ▶️ YouTube v0.1.0(1004) response.beta");
+const $ = new ENV("🍟 GetSomeFries: ▶️ YouTube v0.1.0(1031) response.beta");
 
 /***************** Processing *****************/
 // 解构URL
@@ -19,7 +19,7 @@ const FORMAT = ($response.headers?.["Content-Type"] ?? $response.headers?.["cont
 $.log(`⚠ FORMAT: ${FORMAT}`, "");
 !(async () => {
 	const { Settings, Caches, Configs } = setENV("GetSomeFries", "YouTube", Database);
-	$.log(`⚠ ${$.name}`, `Settings.Switch: ${Settings?.Switch}`, "");
+	$.log(`⚠ Settings.Switch: ${Settings?.Switch}`, "");
 	switch (Settings.Switch) {
 		case true:
 		default:
@@ -38,47 +38,70 @@ $.log(`⚠ FORMAT: ${FORMAT}`, "");
 				case "application/vnd.apple.mpegurl":
 				case "audio/mpegurl":
 					//body = M3U8.parse($response.body);
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
 					//$response.body = M3U8.stringify(body);
 					break;
-				case "text/xml":
 				case "text/html":
+					$.log(`🚧 $response.body: ${$response.body}`, "");
+					document.write($response.body);
+					// 路径判断
+					switch (PATH) {
+						case "/watch":
+							//$response.body = $response.body.replace('"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH",','"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH","controlsType":"3",');
+							//$response.body = $response.body.replace('"platform":"DESKTOP",','"platform":"MOBILE",');
+							//let script = document.querySelector("head > script:nth-child(31)");
+							let script = document.head.querySelector("script:nth-child(22)");
+							//$.log(`🚧 script: ${script.outerHTML}`, "");
+							$.log(`🚧 script: ${script.innerHTML}`, "");
+							const CFGRegex = /ytcfg\.set\((?<ytcfg>{.+})\)/;
+							let ytcfg = script.innerHTML.match(CFGRegex)?.groups.ytcfg || null;
+							$.log(`🚧 ytcfg: ${ytcfg}`, "");
+							//document.getElementById("movie_player").classList.add("ytp-hide-controls");
+							//document.getElementById("movie_player").classList.add("ytp-native-controls");
+							//document.getElementById("movie_player").classList.remove("ytp-branding-shown");
+							//document.getElementsByTagName("video")[0].controls = true;
+							//document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls").remove()
+							//document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container")
+							//class="branding-img-container ytp-button"
+							//#movie_player > div:nth-child(30) > div > button
+							//let script = body.querySelector("body > script:nth-child(10)").textContent;
+							//let script = document.createElement("script");
+							//$.log(`🚧 nonce: ${document.querySelector('head > script:nth-child(1)').nonce}`, "");
+							//script.nonce = document.querySelector('head > script:nth-child(1)').nonce;
+							//script.innerHTML = "window.yt.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].controlsType = 3; window.yt.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].platform = 'MOBILE';";
+							//$.log(`🚧 newScript: ${script.outerHTML}`, "");
+							//let newScript = document.adoptNode(script);
+							//for (const child of document.documentElement.childNodes) {
+							//	$.log(`🚧 child: ${child.outerHTML}`, "")
+							//};
+							//$.log(`🚧 body: ${document.getElementsByTagName('body')[0].outerHTML}`, "");
+							//document.body.prepend(script);
+							//body.querySelector("body > script:nth-child(10)").textContent = "window.yt.config_.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].controlsType = 3; window.yt.config_.WEB_PLAYER_CONTEXT_CONFIGS['WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH'].platform = 'MOBILE';" + script;
+							break;
+					};
+					$response.body = document.documentElement.outerHTML;
+					break;
+				case "text/xml":
 				case "text/plist":
 				case "application/xml":
 				case "application/plist":
 				case "application/x-plist":
-					$.log(`🚧 ${$.name}`, `body: ${$response.body}`, "");
-					//body = new DOMParser().parseFromString($response.body, FORMAT);
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
-					// 路径判断
-					switch (PATH) {
-						case "/watch":
-							$response.body = $response.body.replace('"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH",','"contextId":"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_WATCH","controlsType":"3",');
-							$response.body = $response.body.replace('"platform":"DESKTOP",','"platform":"MOBILE",');
-							//let script = body.querySelector("head > script:nth-child(22)");
-							//$.log(`🚧 ${$.name}`, `script: ${JSON.stringify(script)}`, "");
-							//$.log(`🚧 ${$.name}`, `cgiData: ${JSON.stringify(cgiData ?? undefined)}`, "");
-							//document.getElementById("movie_player").classList.add("ytp-hide-controls");
-							//document.getElementById("movie_player").classList.add("ytp-native-controls");
-							//document.getElementById("movie_player").classList.remove("ytp-branding-shown");
-							//document.getElementById("video").classList.remove("ytp-branding-shown");
-							//class="branding-img-container ytp-button"
-							//#movie_player > div:nth-child(30) > div > button
-							break;
-					};
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
-					//$response.body = new XMLSerializer().serializeToString(body);
+					//$.log(`🚧 $response.body: ${$response.body}`, "");
+					body = new DOMParser().parseFromString($response.body, FORMAT);
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
+					$response.body = new XMLSerializer().serializeToString(body);
+					//$.log(`🚧 $response.body: ${$response.body}`, "");
 					break;
 				case "text/vtt":
 				case "application/vtt":
 					//body = VTT.parse($response.body);
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
 					//$response.body = VTT.stringify(body);
 					break;
 				case "text/json":
 				case "application/json":
 					//body = JSON.parse($request.body ?? "{}");
-					//$.log(`🚧 ${$.name}`, `body: ${JSON.stringify(body)}`, "");
+					//$.log(`🚧 body: ${JSON.stringify(body)}`, "");
 					//$request.body = JSON.stringify(body);
 					break;
 				case "application/protobuf":
